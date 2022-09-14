20701b0aeb999d220a93ee0dfc9682fc0afdc84f
HoodDoob <hubert.kurkiewicz@gmail.com>
2022-09-15 00:43:12 +0200

    everything working but the event listeeners

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 "use strict";
 const url = "https://petlatkea.dk/2021/hogwarts/students.json";
-// window.addEventListener("DOMContentLoaded", start);
+window.addEventListener("DOMContentLoaded", start);
+
 start();
+
 const Student = {
+  index: "",
   firstName: "-default name",
   lastName: "-no description",
   middleName: "-unknown-",
@@ -13,26 +16,93 @@ const Student = {
   imageName: "",
 };
 let allStudents = [];
-function start() {
+let index = 0;
+async function start() {
   document.querySelector("#filter").addEventListener("change", filter);
   // console.log("ready");
 
-  loadJSON();
+  await loadJSON();
 }
+function addButtons(student) {
+  // console.log(allStudents);
+  document
+    .querySelector(`button${student.index}`)
+    .addEventListener("click", test1);
+
+  // document.querySelector(`#popup${actor.index}`).classList.toggle("hidden");
+}
+function test1() {
+  console.log("clicked");
+}
+// MODEL    MODEL     MODEL     MODEL
+// MODEL    MODEL     MODEL     MODEL
 
 async function loadJSON() {
   const response = await fetch(url);
   const jsonData = await response.json();
   // when loaded, prepare data objects
-  prepareObjects(jsonData);
+  await prepareObjects(jsonData);
+  // addButtons(allStudents);
 }
 
 function prepareObjects(jsonData) {
   allStudents = jsonData.map(prepareObject);
-  // console.table(allStudents);
+  console.table(allStudents);
 
   // TODO: This might not be the function we want to call first
   displayList(allStudents);
+}
+function prepareObject(jsonObject) {
+  const student = Object.create(Student);
+  // extrat data from json Object
+  let gender = jsonObject.gender.trim();
+  let fullName = jsonObject.fullname.trim();
+  let studentName = fullName.substring("", fullName.indexOf(" "));
+  let house = jsonObject.house.trim();
+  let studentLastName = fullName.substring(fullName.lastIndexOf(" ")).trim();
+  let middleName = fullName
+    .substring(fullName.indexOf(" "), fullName.lastIndexOf(" "))
+    .trim();
+
+  if (middleName.includes(`"`)) {
+    student.nickName = fullName
+      .substring(fullName.indexOf(" "), fullName.lastIndexOf(" "))
+      .replaceAll(`"`, "")
+      .trim();
+    middleName = "";
+  } else {
+    student.nickName = undefined;
+  }
+  studentLastName = capitalize(studentLastName);
+  house = capitalize(house);
+  let newStudentName = capitalize(studentName);
+  middleName = capitalize(middleName);
+  let imageName;
+
+  if (studentLastName == "Patil") {
+    imageName = `${studentLastName.toLowerCase()}_${studentName.toLowerCase()}.png`;
+  } else if (studentLastName.includes("-")) {
+    imageName = "fletchley_j.png";
+  } else {
+    imageName = `${studentLastName.toLowerCase()}_${studentName[0]}.png`;
+  }
+
+  // put cleaned data into newly created object
+  student.firstName = newStudentName;
+  student.gender = gender;
+  student.lastName = studentLastName;
+  student.middleName = middleName;
+  // student.nickName = nickName;
+  student.house = house;
+  student.imageName = imageName;
+  student.index = index++;
+  return student;
+}
+// CONTROLLER    CONTROLLER     CONTROLLER     CONTROLLER
+// CONTROLLER    CONTROLLER     CONTROLLER     CONTROLLER
+
+function popUp() {
+  console.log("pop up");
 }
 function filter() {
   let filterValue = document.querySelector("#filter").value;
@@ -62,73 +132,25 @@ function isBoy(student) {
     return false;
   }
 }
-
-function prepareObject(jsonObject) {
-  const student = Object.create(Student);
-  // extrat data from json Object
-
-  let gender = jsonObject.gender.trim();
-  let fullName = jsonObject.fullname.trim();
-  let studentName = fullName.substring("", fullName.indexOf(" "));
-  let house = jsonObject.house.trim();
-  let studentLastName = fullName.substring(fullName.lastIndexOf(" ")).trim();
-  let middleName = fullName
-    .substring(fullName.indexOf(" "), fullName.lastIndexOf(" "))
-    .trim();
-
-  if (middleName.includes(`"`)) {
-    student.nickName = fullName
-      .substring(fullName.indexOf(" "), fullName.lastIndexOf(" "))
-      .replaceAll(`"`, "")
-      .trim();
-    middleName = "";
-  }
-  studentLastName = capitalize(studentLastName);
-  house = capitalize(house);
-  let newStudentName = capitalize(studentName);
-  middleName = capitalize(middleName);
-  let imageName;
-
-  if (studentLastName == "Patil") {
-    imageName = `${studentLastName.toLowerCase()}_${studentName.toLowerCase()}.png`;
-  } else if (studentLastName.includes("-")) {
-    imageName = "fletchley_j.png";
-  } else {
-    imageName = `${studentLastName.toLowerCase()}_${studentName[0]}.png`;
-  }
-
-  // put cleaned data into newly created object
-  student.firstName = newStudentName;
-  student.gender = gender;
-  student.lastName = studentLastName;
-  student.middleName = middleName;
-  // student.nickName = nickName;
-  student.house = house;
-  student.imageName = imageName;
-
-  return student;
-}
-// TODO: Create new object with cleaned data - and store that in the allAnimals array
-// TODO: MISSING CODE HERE !!!
-
-// console.log(student);
-
 function capitalize(str) {
   if (str != "") return str[0].toUpperCase() + str.substring(1).toLowerCase();
 }
 
-function displayList(allStudents) {
+// VIEW     VIEW     VIEW     VIEW
+// VIEW     VIEW     VIEW     VIEW
+
+async function displayList(allStudents) {
   // clear the list
-  document.querySelector(".general_students").innerHTML = "";
+  // document.querySelector(".general_students").innerHTML = "";
   // build a new list
-  // console.log("array in displaylist: ", allStudents);
-  allStudents.forEach(displayStudents);
+  await allStudents.forEach((student) => (student.index = index++));
+  await allStudents.forEach(displayStudents);
+  allStudents.forEach(addButtons);
 }
 
 function displayStudents(student) {
   // create clone
   const clone = document.querySelector("template").content.cloneNode(true);
-
   // set clone data
   if (student.middleName != undefined) {
     clone.querySelector(
@@ -139,11 +161,18 @@ function displayStudents(student) {
   }
 
   clone.querySelector("#surname").textContent = student.lastName;
-  clone.querySelector("#nickname").textContent = student.nickName;
+  if (student.nickName != undefined) {
+    clone.querySelector(".student_nickname").innerHTML = "Nickname";
+    clone.querySelector("#nickname").textContent = student.nickName;
+  }
+
   clone.querySelector(
     ".student_picture2"
   ).src = `imgStudents/${student.imageName}`;
-
+  // adding unique ID to each template clone
+  clone.querySelector("button").setAttribute("id", `index${student.index}`);
+  clone.querySelector(".popup").setAttribute("id", `index${student.index}`);
+  // clone
   // append clone to list
   document.querySelector(".general_students").appendChild(clone);
 }
